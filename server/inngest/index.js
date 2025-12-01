37c05c94cf74f3edc90ee5c2767cd76440a3e804
Faique <faique.official695@gmail.com>
2025-12-02 02:43:01 +0530

    add Inngest function to save workspace member data to a database

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -91,5 +91,17 @@ const syncWorkspaceDeletion = inngest.createFunction({ id: "delete-workspace-wit
     });
 });
 
+// Inngest Function to save workspace member data to a database
+const syncWorkspaceMemberCreation = inngest.createFunction({ id: "sync-workspace-member-from-clerk" }, { event: "clerk/organizationInvitation.accepted" }, async ({ event }) => {
+    const { data } = event;
+    await prisma.workspaceMember.create({
+        data: {
+            userId: data.user_id,
+            workspaceId: data.organization_id,
+            role: String(data.role_name).toUpperCase(),
+        },
+    });
+});
+
 // Create an empty array where we'll export future Inngest functions
-export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation, syncWorkspaceCreation, syncWorkspaceUpdation, syncWorkspaceDeletion];
+export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation, syncWorkspaceCreation, syncWorkspaceUpdation, syncWorkspaceDeletion, syncWorkspaceMemberCreation];
