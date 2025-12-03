68687826694f35737370c5ec134c7784242b3c53
Faique <faique.official695@gmail.com>
2025-12-03 21:24:39 +0530

    add task management routes and controller for creating, updating, and deleting tasks

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
 import workspaceRouter from "./routes/workspaceRoutes.js";
 import { protect } from './middlewares/authMiddleware.js';
+import taskRouter from "./routes/taskRoutes.js";
+import projectRouter from "./routes/projectRoutes.js";
 
 const app = express();
 
@@ -19,6 +21,7 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 // Routes
 app.use("/api/workspaces", protect, workspaceRouter);
 app.use("/api/projects", protect, projectRouter);
+app.use("/api/tasks", protect, taskRouter);
 
 const PORT = process.env.PORT || 5000;
 
